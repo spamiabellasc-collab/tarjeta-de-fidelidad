3bc1f9a7a53d5ddc0c712cfb7615bd43dcc8701d
Sol Anahi <solanahi.cg@gmail.com>
2026-04-06 09:27:49 -0400

    fix mobile layout sidebar responsive

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,10 +4,12 @@ import Topbar from "./Topbar";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-[#fff9f8] flex w-full overflow-x-hidden">
-      <Sidebar />
+    <div className="min-h-screen bg-[#fff9f8] w-full overflow-x-hidden lg:flex">
+      <div className="hidden lg:block lg:shrink-0">
+        <Sidebar />
+      </div>
 
-      <div className="flex-1 min-w-0 flex flex-col">
+      <div className="min-w-0 flex-1 flex flex-col">
         <Topbar />
 
         <main className="flex-1 min-w-0 p-3 sm:p-4 md:p-5 lg:p-6">
